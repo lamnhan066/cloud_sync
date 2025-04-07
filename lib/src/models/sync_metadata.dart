c97a8b2fa6ca7fb8ebeb529705a1ed3f59308bd2
lamnhan066 <hello@lamnhan.dev>
2025-04-07 16:51:31 +0700

    Add isDeleted field and update SyncMetadata methods for improved functionality

--- lib/src/models/sync_metadata.dart
+++ lib/src/models/sync_metadata.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 /// A model class representing metadata for synchronization.
 ///
 /// This class holds information about an entity's unique identifier and
@@ -10,8 +12,52 @@ class SyncMetadata {
   /// The timestamp of the last modification.
   final DateTime modifiedAt;
 
+  /// Indicates whether the metadata has been deleted.
+  final bool isDeleted;
+
   /// Creates a new instance of [SyncMetadata].
   ///
   /// Both [id] and [modifiedAt] are required parameters.
-  SyncMetadata({required this.id, required this.modifiedAt});
+  SyncMetadata({
+    required this.id,
+    required this.modifiedAt,
+    this.isDeleted = false,
+  });
+
+  SyncMetadata copyWith({
+    String? id,
+    DateTime? modifiedAt,
+    bool? isDeleted,
+  }) {
+    return SyncMetadata(
+      id: id ?? this.id,
+      modifiedAt: modifiedAt ?? this.modifiedAt,
+      isDeleted: isDeleted ?? this.isDeleted,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'modifiedAt': modifiedAt.toIso8601String(),
+      'isDeleted': isDeleted,
+    };
+  }
+
+  factory SyncMetadata.fromMap(Map<String, dynamic> map) {
+    return SyncMetadata(
+      id: map['id'] ?? '',
+      modifiedAt: DateTime.parse(map['modifiedAt']),
+      isDeleted: map['isDeleted'] ?? false,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory SyncMetadata.fromJson(String source) =>
+      SyncMetadata.fromMap(json.decode(source));
+
+  @override
+  String toString() =>
+      'SyncMetadata(id: $id, modifiedAt: $modifiedAt, isDeleted: $isDeleted)';
 }
