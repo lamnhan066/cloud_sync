3d5675b6c1f0864bc839f7f492d9bd2e25d2eb96
lamnhan066 <hello@lamnhan.dev>
2025-04-07 11:06:37 +0700

    Refactor cloud_sync_test for improved clarity and consistency in test cases and metadata handling

--- test/cloud_sync_test.dart
+++ test/cloud_sync_test.dart
@@ -12,24 +12,12 @@ void main() {
 
   setUp(() {
     localMetadataList = [
-      SyncMetadata(
-        id: '1',
-        modifiedAt: DateTime(2023, 1, 1),
-      ),
-      SyncMetadata(
-        id: '2',
-        modifiedAt: DateTime(2023, 1, 2),
-      ),
+      SyncMetadata(id: '1', modifiedAt: DateTime(2023, 1, 1)),
+      SyncMetadata(id: '2', modifiedAt: DateTime(2023, 1, 2)),
     ];
     cloudMetadataList = [
-      SyncMetadata(
-        id: '2',
-        modifiedAt: DateTime(2023, 1, 1),
-      ),
-      SyncMetadata(
-        id: '3',
-        modifiedAt: DateTime(2023, 1, 3),
-      ),
+      SyncMetadata(id: '2', modifiedAt: DateTime(2023, 1, 1)),
+      SyncMetadata(id: '3', modifiedAt: DateTime(2023, 1, 3)),
     ];
     localDetails = {
       '1': [108, 111, 99, 97, 108, 70, 105, 108, 101, 49],
@@ -59,27 +47,45 @@ void main() {
   });
 
   group('sync', () {
-    test('sync should synchronize missing or outdated details to the cloud',
+    test(
+        'sync should synchronize missing or outdated details between cloud and local storage',
         () async {
       final progressStates = <SyncState>[];
       await cloudSync.sync(progressCallback: progressStates.add);
 
-      expect(progressStates, contains(isA<WritingDetailToCloud>()));
-      expect(progressStates, contains(isA<SyncCompleted>()));
+      expect(
+          progressStates,
+          containsAllInOrder([
+            isA<WritingDetailToCloud>(),
+            isA<WritingDetailToLocal>(),
+            isA<SyncCompleted>(),
+          ]));
       expect(cloudDetails.containsKey('1'), isTrue);
       expect(cloudDetails['1'], equals(localDetails['1']));
+      expect(localDetails.containsKey('3'), isTrue);
+      expect(localDetails['3'], equals(cloudDetails['3']));
     });
 
-    test(
-        'sync should synchronize missing or outdated details to the local storage',
-        () async {
+    test('sync should skip already synchronized details', () async {
+      localMetadataList = [
+        SyncMetadata(id: '1', modifiedAt: DateTime(2023, 1, 1))
+      ];
+      cloudMetadataList = [
+        SyncMetadata(id: '1', modifiedAt: DateTime(2023, 1, 1))
+      ];
+      localDetails = {
+        '1': [115, 97, 109, 101, 70, 105, 108, 101]
+      };
+      cloudDetails = {
+        '1': [115, 97, 109, 101, 70, 105, 108, 101]
+      };
+
       final progressStates = <SyncState>[];
       await cloudSync.sync(progressCallback: progressStates.add);
 
-      expect(progressStates, contains(isA<WritingDetailToLocal>()));
       expect(progressStates, contains(isA<SyncCompleted>()));
-      expect(localDetails.containsKey('3'), isTrue);
-      expect(localDetails['3'], equals(cloudDetails['3']));
+      expect(progressStates, isNot(contains(isA<WritingDetailToLocal>())));
+      expect(progressStates, isNot(contains(isA<WritingDetailToCloud>())));
     });
 
     test('sync should not run if already in progress', () async {
@@ -90,7 +96,8 @@ void main() {
       expect(progressStates, contains(isA<AlreadyInProgress>()));
     });
 
-    test('sync should handle errors during synchronization', () async {
+    test('sync should handle errors during synchronization gracefully',
+        () async {
       cloudSync = CloudSync(
         fetchLocalMetadataList: () async => throw Exception('Test error'),
         fetchCloudMetadataList: () async => cloudMetadataList,
@@ -106,91 +113,33 @@ void main() {
       expect(progressStates, contains(isA<SyncError>()));
     });
 
-    test('sync should handle errors during synchronization', () async {
-      cloudSync = CloudSync(
-        fetchLocalMetadataList: () async => throw Exception('Test error'),
-        fetchCloudMetadataList: () async => cloudMetadataList,
-        fetchLocalDetail: (metadata) async => localDetails[metadata.id]!,
-        fetchCloudDetail: (metadata) async => cloudDetails[metadata.id]!,
-        writeDetailToLocal: (metadata, detail) async {},
-        writeDetailToCloud: (metadata, detail) async {},
-      );
-
-      await expectLater(
-        () => cloudSync.sync(),
-        throwsException,
-      );
-    });
-
-    test('sync should skip details that are already up to date', () async {
-      localMetadataList = [
-        SyncMetadata(
-          id: '1',
-          modifiedAt: DateTime(2023, 1, 1),
-        ),
-      ];
-      cloudMetadataList = [
-        SyncMetadata(
-          id: '1',
-          modifiedAt: DateTime(2023, 1, 1),
-        ),
-      ];
-      localDetails = {
-        '1': [115, 97, 109, 101, 70, 105, 108, 101],
-      };
-      cloudDetails = {
-        '1': [115, 97, 109, 101, 70, 105, 108, 101],
-      };
+    test('sync should skip syncing with empty data', () async {
+      // Clear metadata and details to simulate empty data.
+      localMetadataList.clear();
+      cloudMetadataList.clear();
+      localDetails.clear();
+      cloudDetails.clear();
 
       final progressStates = <SyncState>[];
       await cloudSync.sync(progressCallback: progressStates.add);
 
-      expect(progressStates, isNot(contains(isA<WritingDetailToLocal>())));
-      expect(progressStates, isNot(contains(isA<WritingDetailToCloud>())));
+      // Ensure the sync completes without any syncing happening.
       expect(progressStates, contains(isA<SyncCompleted>()));
-    });
 
-    test('sync should handle two consecutive sync operations', () async {
-      final firstSyncProgressStates = <SyncState>[];
-      final secondSyncProgressStates = <SyncState>[];
-
-      await cloudSync.sync(progressCallback: firstSyncProgressStates.add);
-      await cloudSync.sync(progressCallback: secondSyncProgressStates.add);
-
-      expect(firstSyncProgressStates, contains(isA<SyncCompleted>()));
-      expect(secondSyncProgressStates, contains(isA<SyncCompleted>()));
-      expect(secondSyncProgressStates,
-          isNot(contains(isA<WritingDetailToLocal>())));
-      expect(secondSyncProgressStates,
-          isNot(contains(isA<WritingDetailToCloud>())));
-    });
-
-    test('sync should handle multiple details with different states', () async {
-      localMetadataList.add(SyncMetadata(
-        id: '4',
-        modifiedAt: DateTime(2023, 1, 4),
-      ));
-      localDetails['4'] = [108, 111, 99, 97, 108, 70, 105, 108, 101, 52];
-
-      final progressStates = <SyncState>[];
-      await cloudSync.sync(progressCallback: progressStates.add);
-
-      expect(progressStates, contains(isA<WritingDetailToCloud>()));
-      expect(progressStates, contains(isA<WritingDetailToLocal>()));
-      expect(progressStates, contains(isA<SyncCompleted>()));
-      expect(cloudDetails.containsKey('4'), isTrue);
-      expect(cloudDetails['4'], equals(localDetails['4']));
+      // Check that both local and cloud details are empty.
+      expect(localDetails.isEmpty, isTrue);
+      expect(cloudDetails.isEmpty, isTrue);
     });
   });
 
   group('autoSync', () {
-    test('autoSync should periodically trigger sync operations', () async {
-      final progressStates = <SyncState>[];
+    test(
+        'autoSync should periodically trigger sync operations and stop after a set time',
+        () async {
       final syncCallCounts = <int>[];
-
       cloudSync = CloudSync(
         fetchLocalMetadataList: () async {
-          syncCallCounts.add(1);
+          syncCallCounts.add(syncCallCounts.length + 1);
           return localMetadataList;
         },
         fetchCloudMetadataList: () async => cloudMetadataList,
@@ -202,45 +151,27 @@ void main() {
 
       cloudSync.autoSync(
         interval: const Duration(milliseconds: 100),
-        progressCallback: progressStates.add,
+        progressCallback: (_) {},
       );
 
-      // Wait for multiple sync cycles to complete.
       await Future.delayed(const Duration(milliseconds: 350));
       cloudSync.stopAutoSync();
 
       expect(syncCallCounts.length, greaterThanOrEqualTo(3));
-      expect(progressStates, contains(isA<SyncCompleted>()));
     });
 
     test('stopAutoSync should cancel the periodic sync timer', () async {
-      final progressStates = <SyncState>[];
       final syncCallCounts = <int>[];
-
-      cloudSync = CloudSync(
-        fetchLocalMetadataList: () async {
-          syncCallCounts.add(1);
-          return localMetadataList;
-        },
-        fetchCloudMetadataList: () async => cloudMetadataList,
-        fetchLocalDetail: (metadata) async => localDetails[metadata.id]!,
-        fetchCloudDetail: (metadata) async => cloudDetails[metadata.id]!,
-        writeDetailToLocal: (metadata, detail) async {},
-        writeDetailToCloud: (metadata, detail) async {},
-      );
 
       cloudSync.autoSync(
         interval: const Duration(milliseconds: 100),
-        progressCallback: progressStates.add,
+        progressCallback: (_) {},
       );
 
-      // Stop auto-sync after a short delay.
       await Future.delayed(const Duration(milliseconds: 150));
       cloudSync.stopAutoSync();
 
-      // Wait to ensure no further syncs are triggered.
       await Future.delayed(const Duration(milliseconds: 200));
-
       expect(syncCallCounts.length, lessThanOrEqualTo(2));
     });
   });
