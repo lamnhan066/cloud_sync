705f62793ef45508445eeba571367fd5b7369a94
lamnhan066 <hello@lamnhan.dev>
2025-04-09 10:44:42 +0700

    SyncMetadata should not contains `copyWith` and `json`

--- lib/src/models/sync_metadata.dart
+++ lib/src/models/sync_metadata.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 /// A model class representing metadata for synchronization.
 ///
 /// This class holds information about an entity's unique identifier,
@@ -19,24 +17,6 @@ class SyncMetadata {
     this.isDeleted = false,
   });
 
-  /// Creates a new instance of [SyncMetadata] from a [Map].
-  ///
-  /// The [map] must contain the keys `id` and `modifiedAt`.
-  /// The `isDeleted` key is optional and defaults to `false` if not provided.
-  factory SyncMetadata.fromMap(Map<String, dynamic> map) {
-    return SyncMetadata(
-      id: (map['id'] ?? '') as String,
-      modifiedAt: DateTime.parse(map['modifiedAt'] as String),
-      isDeleted: (map['isDeleted'] as bool?) ?? false,
-    );
-  }
-
-  /// Creates a new instance of [SyncMetadata] from a JSON string.
-  ///
-  /// The [source] string must represent a valid JSON object.
-  factory SyncMetadata.fromJson(String source) =>
-      SyncMetadata.fromMap(json.decode(source) as Map<String, dynamic>);
-
   /// The unique identifier for the metadata.
   final String id;
 
@@ -45,39 +25,4 @@ class SyncMetadata {
 
   /// Indicates whether the metadata has been marked as deleted.
   final bool isDeleted;
-
-  /// Creates a copy of the current [SyncMetadata] instance with updated fields.
-  ///
-  /// Any field not provided will retain its current value.
-  SyncMetadata copyWith({
-    String? id,
-    DateTime? modifiedAt,
-    bool? isDeleted,
-  }) {
-    return SyncMetadata(
-      id: id ?? this.id,
-      modifiedAt: modifiedAt ?? this.modifiedAt,
-      isDeleted: isDeleted ?? this.isDeleted,
-    );
-  }
-
-  /// Converts the [SyncMetadata] instance to a [Map].
-  ///
-  /// The resulting map contains the keys `id`, `modifiedAt`, and `isDeleted`.
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'modifiedAt': modifiedAt.toIso8601String(),
-      'isDeleted': isDeleted,
-    };
-  }
-
-  /// Converts the [SyncMetadata] instance to a JSON string.
-  ///
-  /// The JSON string represents the metadata as a serialized object.
-  String toJson() => json.encode(toMap());
-
-  @override
-  String toString() =>
-      'SyncMetadata(id: $id, modifiedAt: $modifiedAt, isDeleted: $isDeleted)';
 }
