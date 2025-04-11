8e1400d59f7d2f63b9f1ef686052e1ffc2512659
lamnhan066 <hello@lamnhan.dev>
2025-04-11 20:09:23 +0700

    Remove [de]serialization for `SyncMetadata`

--- lib/cloud_sync.dart
+++ lib/cloud_sync.dart
@@ -1,5 +1,4 @@
 export 'src/cloud_sync.dart';
-export 'src/extensions/sync_metadata_serialization.dart';
 export 'src/models/sync_adapter.dart';
 export 'src/models/sync_errors.dart';
 export 'src/models/sync_metadata.dart';
