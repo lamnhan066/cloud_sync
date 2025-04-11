8b6915bed30ec43cc46d57832626f3cb5bc75200
lamnhan066 <hello@lamnhan.dev>
2025-04-11 22:07:55 +0700

    Add MockSerializableSyncAdapter and tests for metadata serialization and deserialization

--- test/cloud_sync_test.dart
+++ test/cloud_sync_test.dart
@@ -116,6 +116,23 @@ class MockSyncAdapter extends SyncAdapter<SyncMetadata, MockData> {
   }
 }
 
+class MockSerializableSyncAdapter
+    extends SerializableSyncAdapter<SyncMetadata, MockData> {
+  MockSerializableSyncAdapter({
+    required super.metadataToJson,
+    required super.metadataFromJson,
+  });
+
+  @override
+  Future<List<SyncMetadata>> fetchMetadataList() async => [];
+
+  @override
+  Future<MockData> fetchDetail(SyncMetadata metadata) async => MockData('');
+
+  @override
+  Future<void> save(SyncMetadata metadata, MockData detail) async {}
+}
+
 // Custom equality matcher for SyncState types
 class IsSyncStateType extends Matcher {
   IsSyncStateType(this.expectedType);
@@ -1017,6 +1034,89 @@ void main() {
       expect(fromJson.id, testMetadata.id);
       expect(fromJson.modifiedAt, testMetadata.modifiedAt);
       expect(fromJson.isDeleted, testMetadata.isDeleted);
+    });
+  });
+
+  group('SerializableSyncAdapter Tests', () {
+    late SerializableSyncAdapter<SyncMetadata, MockData> adapter;
+
+    setUp(() {
+      adapter = MockSerializableSyncAdapter(
+        metadataToJson: (metadata) => json.encode({
+          'id': metadata.id,
+          'modifiedAt': metadata.modifiedAt.toIso8601String(),
+          'isDeleted': metadata.isDeleted,
+        }),
+        metadataFromJson: (jsonStr) {
+          final map = json.decode(jsonStr) as Map<String, dynamic>;
+          return SyncMetadata(
+            id: map['id'] as String,
+            modifiedAt: DateTime.parse(map['modifiedAt'] as String),
+            isDeleted: map['isDeleted'] as bool? ?? false,
+          );
+        },
+      );
+    });
+
+    test('metadataToJson serializes metadata correctly', () {
+      final metadata = SyncMetadata(
+        id: 'test-id',
+        modifiedAt: DateTime.parse('2024-04-10T12:00:00.000Z'),
+        isDeleted: true,
+      );
+
+      final jsonStr = adapter.metadataToJson(metadata);
+
+      expect(
+        jsonStr,
+        json.encode({
+          'id': 'test-id',
+          'modifiedAt': '2024-04-10T12:00:00.000Z',
+          'isDeleted': true,
+        }),
+      );
+    });
+
+    test('metadataFromJson deserializes metadata correctly', () {
+      final jsonStr = json.encode({
+        'id': 'test-id',
+        'modifiedAt': '2024-04-10T12:00:00.000Z',
+        'isDeleted': true,
+      });
+
+      final metadata = adapter.metadataFromJson(jsonStr);
+
+      expect(metadata.id, 'test-id');
+      expect(metadata.modifiedAt, DateTime.parse('2024-04-10T12:00:00.000Z'));
+      expect(metadata.isDeleted, true);
+    });
+
+    test('metadataFromJson handles missing isDeleted field gracefully', () {
+      final jsonStr = json.encode({
+        'id': 'test-id',
+        'modifiedAt': '2024-04-10T12:00:00.000Z',
+      });
+
+      final metadata = adapter.metadataFromJson(jsonStr);
+
+      expect(metadata.id, 'test-id');
+      expect(metadata.modifiedAt, DateTime.parse('2024-04-10T12:00:00.000Z'));
+      expect(metadata.isDeleted, false);
+    });
+
+    test('metadataToJson and metadataFromJson are inverses', () {
+      final originalMetadata = SyncMetadata(
+        id: 'test-id',
+        modifiedAt: DateTime.parse('2024-04-10T12:00:00.000Z'),
+        isDeleted: true,
+      );
+
+      final jsonStr = adapter.metadataToJson(originalMetadata);
+      final deserializedMetadata = adapter.metadataFromJson(jsonStr);
+
+      expect(deserializedMetadata.id, originalMetadata.id);
+      expect(deserializedMetadata.modifiedAt, originalMetadata.modifiedAt);
+      expect(deserializedMetadata.isDeleted, originalMetadata.isDeleted);
     });
   });
 }
