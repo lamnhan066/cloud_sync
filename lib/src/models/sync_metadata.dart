edc731208e08de8daca0f4a3354c9ca3be5a3a9f
lamnhan066 <hello@lamnhan.dev>
2025-04-11 21:59:15 +0700

    Add SerializableSyncMetadata class for improved serialization and deserialization

--- lib/src/models/sync_metadata.dart
+++ lib/src/models/sync_metadata.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 /// A model class representing metadata used for synchronization.
 ///
 /// This class encapsulates information about an entity's unique identifier,
@@ -25,4 +27,43 @@ class SyncMetadata {
   ///
   /// If `true`, the entity is considered deleted. Defaults to `false`.
   final bool isDeleted;
+}
+
+/// A serializable version of [SyncMetadata] for encoding/decoding purposes,
+/// typically used for saving to or reading from storage like JSON or databases.
+class SerializableSyncMetadata extends SyncMetadata {
+  /// Constructor that initializes fields from the [SyncMetadata] superclass.
+  /// [id] and [modifiedAt] are required, while [isDeleted] is optional.
+  SerializableSyncMetadata({
+    required super.id,
+    required super.modifiedAt,
+    super.isDeleted,
+  });
+
+  /// Creates an instance of [SerializableSyncMetadata] from a Map.
+  factory SerializableSyncMetadata.fromMap(Map<String, dynamic> map) {
+    return SerializableSyncMetadata(
+      id: map['id'] as String,
+      modifiedAt: DateTime.parse(map['modifiedAt'] as String),
+      isDeleted: map['isDeleted'] as bool,
+    );
+  }
+
+  /// Creates an instance of [SerializableSyncMetadata] from a JSON string.
+  factory SerializableSyncMetadata.fromJson(String source) =>
+      SerializableSyncMetadata.fromMap(
+        json.decode(source) as Map<String, dynamic>,
+      );
+
+  /// Converts the object to a Map, making it suitable for encoding or storage.
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'modifiedAt': modifiedAt.toIso8601String(),
+      'isDeleted': isDeleted,
+    };
+  }
+
+  /// Converts the object to a JSON string using [toMap].
+  String toJson() => json.encode(toMap());
 }
